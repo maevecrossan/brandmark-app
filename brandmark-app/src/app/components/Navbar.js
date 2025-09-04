9b3ecc4e99f2abea26d525f43975a73bc6fd67e3
Maeve Crossan <maevecrossan@gmail.com>
2025-09-04 14:25:33 +0100

    Update navbar to use flowbite template w burger menu. Update link colours.

--- brandmark-app/src/app/components/Navbar.js
+++ brandmark-app/src/app/components/Navbar.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Navbar() {
 	return (
-		<nav className="font-sans flex items-center justify-between p-6 w-full bg-white shadow-md z-10">
-			<div className="text-lg font-bold">
-				<a href="/" className="flex items-center text-2xl">
+		<nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 focus:ring-2 focus:ring-gray-200">
+				<a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
 					<Image
 						src="/assets/logo-no-bg.png"
 						alt="Brandmark Logo"
@@ -12,15 +12,35 @@ export default function Navbar() {
 						height={32}
 						className="inline-block mr-2"
 					/>
-					Brandmark
+					<span className="text-2xl font-semibold whitespace-nowrap">Brandmark</span>
 				</a>
-			</div>
-			<div className="flex items-center justify-center gap-6 mx-auto">
-				<a href="/" className="hover:underline">Home</a>
-				<a href="/about" className="hover:underline">About</a>
-				<a href="/portfolio" className="hover:underline">Portfolio</a>
-				<a href="/projects" className="hover:underline">Projects</a>
-				<a href="/contact" className="hover:underline">Contact</a>
+				<div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+					<button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+						<span className="sr-only">Open main menu</span>
+						<svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+							<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+						</svg>
+					</button>
+				</div>
+				<div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+					<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+						<li>
+							<a href="/" className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:text-teal md:p-0 active:underline" aria-current="page">Home</a>
+						</li>
+						<li>
+							<a href="/about" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal md:p-0 active:underline">About</a>
+						</li>
+						<li>
+							<a href="/portfolio" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal md:p-0 active:underline">Portfolio</a>
+						</li>
+						<li>
+							<a href="/projects" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal md:p-0 active:underline">Projects</a>
+						</li>
+						<li>
+							<a href="/contact" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal md:p-0 active:underline">Contact</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
