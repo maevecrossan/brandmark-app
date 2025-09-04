2a1ba6ec762e88b07e4e0c55594f1eeea28f8c08
Maeve Crossan <maevecrossan@gmail.com>
2025-09-04 11:28:16 +0100

    Update navbar styles. Left align logo and center align links.

--- brandmark-app/src/app/components/Navbar.js
+++ brandmark-app/src/app/components/Navbar.js
@@ -1,14 +1,27 @@
+import Image from "next/image";
+
 export default function Navbar() {
-  return (
-    <nav className="font-sans flex items-center justify-between p-4">
-      <div className="text-lg font-bold">Brandmark</div>
-      <div className="flex items-center gap-4">
-        <a href="/" className="hover:underline">Home</a>
-        <a href="/about" className="hover:underline">About</a>
-        <a href="/portfolio" className="hover:underline">Portfolio</a>
-        <a href="/contact" className="hover:underline">Contact</a>
-        <a href="/projects" className="hover:underline">Projects</a>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="font-sans flex items-center justify-between p-6 w-full bg-white shadow-md z-10">
+			<div className="text-lg font-bold">
+				<a href="/" className="flex items-center text-2xl">
+					<Image
+						src="/assets/logo-no-bg.png"
+						alt="Brandmark Logo"
+						width={32}
+						height={32}
+						className="inline-block mr-2"
+					/>
+					Brandmark
+				</a>
+			</div>
+			<div className="flex items-center justify-center gap-6 mx-auto">
+				<a href="/" className="hover:underline">Home</a>
+				<a href="/about" className="hover:underline">About</a>
+				<a href="/portfolio" className="hover:underline">Portfolio</a>
+				<a href="/projects" className="hover:underline">Projects</a>
+				<a href="/contact" className="hover:underline">Contact</a>
+			</div>
+		</nav>
+	);
 }
--- brandmark-app/src/app/page.js
+++ brandmark-app/src/app/page.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
       <Navbar />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
