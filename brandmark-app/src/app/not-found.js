4d2915a6bf4832668414b46cb974a52e600ccb42
Maeve Crossan <maevecrossan@gmail.com>
2025-09-04 14:57:30 +0100

    Update min-h to 'screen' from 'full'.

--- brandmark-app/src/app/not-found.js
+++ brandmark-app/src/app/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="font-sans grid place-items-center min-h-screen p-8">
+    <div className="font-sans grid place-items-center min-h-full p-8">
       <h1 className="text-4xl font-bold">404 - Not Found</h1>
       <p className="mt-4">Sorry, the page you are looking for does not exist.</p>
     </div>
