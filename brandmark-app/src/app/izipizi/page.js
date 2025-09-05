cd4322c71ed0005fc3bc63f3b1b5d3219d04b5ef
Maeve Crossan <maevecrossan@gmail.com>
2025-09-05 18:55:43 +0100

    Add copy to Izipizi page.

--- brandmark-app/src/app/izipizi/page.js
+++ brandmark-app/src/app/izipizi/page.js
@@ -2,7 +2,12 @@ export default function IzipiziPage() {
 	return (
 		<div>
 			<h1>Izipizi</h1>
-			<p>Welcome to the Izipizi brand page!</p>
+			<p>We want to make as many people smile as possible, by creating glasses that are both affordable and kind to the planet, and that protect your eyes at every time in your life.</p>
+			<p>More than ten years ago we set ourselves the ambitious goal of protecting the eyes of the 2.5 billion longsighted people in the world. And so our READING glasses were born. Our ranges have since grown and now offer protection from the sun, the blue light emitted by electronic screens and the elements faced when practising a sport.</p>
+			<p>We design glasses that put a smile on your face and can be used every day, for every part of life and every activity. However you use them, they all have a common goal: protecting your eyes. Discover our different ranges, all designed to ensure every moment is a happy experience. That means offering models that are accessible when it comes to their price, timeless style and availability. It’s one of the founding pillars of the brand, and one that’s here to stay.</p>
+			<p>For the past five years, reducing our carbon footprint has been a priority, and we are proud to have cut it in half. This result reflects our collective efforts to transform our practices, from product design to logistics, by placing sustainability at the heart of every decision.</p>
+			<p>We work daily to improve our impact by renewing our materials, putting our human project at the core of our business strategy, and supporting charitable organizations.</p>
+			<p>Certified B Corp in 2023, our commitment is recognized by this label of excellence, which drives us towards continuous improvement.</p>
 		</div>
 	);
 }
