e552251db97382dea491cf70cb9105a0c496db6f
Maeve Crossan <maevecrossan@gmail.com>
2025-09-05 19:06:32 +0100

    Add copy to EAT MY SOCKS page.

--- brandmark-app/src/app/eat-my-socks/page.js
+++ brandmark-app/src/app/eat-my-socks/page.js
@@ -2,7 +2,9 @@ export default function EatMySocksPage() {
 	return (
 		<div>
 			<h1>Eat My Socks</h1>
-			<p>Welcome to the Eat My Socks brand page!</p>
+			<p>At EAT MY SOCKS we design unique socks shaped as unexpected objects. From our food collection, which includes a wide range of models such as cheeseburger, sushi, and pancake socks, to our trend line including fun forms like animals, flowers, and other trendy icons, EAT MY SOCKS offers a large collection of creative, edgy, and playful socks, all of which guarantee 100% surprise and are a perfect wow-factor gift!</p>
+			<p>EAT MY SOCKS has a mission to be gender-neutral and inclusive within the fashion industry. All our models are unisex and one size fits all!</p>
+			<p>All the plastic we use at EAT MY SOCKS is 100% recycled and recyclable. In addition, the film lamination that protects all our cartons is made of wood pulp, a biodegradable natural substance that helps you fully recycle your packaging.</p>
 		</div>
 	);
 }
